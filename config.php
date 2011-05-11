bb203fc7779265c53f71754b1c93bc6616e6826e
Mauro Parra-Miranda <mauropm@gmail.com>
2011-05-11 00:54:19 -0500

    added config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+  /* 
+   * DB Configuration 
+   * (c) 2011 Mauro Parra-Miranda  <mauropm@gmail.com>
+   */
+
+  /* 
+   * here you will define the parameters of the db. 
+   */ 
+
+define('DB_HOST','localhost');
+define('DB_NAME','messages');
+define('DB_USER','user');
+define('DB_PASS','password');
+define('API_KEY','yourapikey'); // This is the apikey you should use in each call. 
+
+?>
